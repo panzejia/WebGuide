f3e70e0ed71f1f71dbff16c1f9cd62d4456f0d5d
panzejia <61635380@qq.com>
2017-08-20 16:03:30 +0800

    备份

---
+++ src/main/java/cn/iflin/guide/model/ArticleModel.java
@@ -0,0 +1,65 @@
+package cn.iflin.guide.model;
+
+public class ArticleModel {
+	private String title;
+	private String starttime;
+	private String stoptime;
+	private String content;
+	private String contentNoCode;
+	private String articleId;
+	private String url;
+	/**
+	 * @return the articleId
+	 */
+	public String getArticleId() {
+		return articleId;
+	}
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+	/**
+	 * @param articleId the articleId to set
+	 */
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
+	}
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getStarttime() {
+		return starttime;
+	}
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+	public String getStoptime() {
+		return stoptime;
+	}
+	public void setStoptime(String stoptime) {
+		this.stoptime = stoptime;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getContentNoCode() {
+		return contentNoCode;
+	}
+	public void setContentNoCode(String contentNoCode) {
+		this.contentNoCode = contentNoCode;
+	}
+}
---
+++ src/main/java/cn/iflin/guide/model/MysqlConnection.java
@@ -0,0 +1,34 @@
+package cn.iflin.guide.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/*
+ * 数据库连接
+ */
+public class MysqlConnection {
+	private static final String URL="jdbc:mysql://127.0.0.1:3306/retrievalsystemdb?autoReconnect=true";
+	private static final String USER="root";
+	private static final String PASSWORD="0503";
+	private static Connection conn=null;
+	//使用静态方法，优先运行静态方法
+	static {
+		try {
+			//加载驱动程序
+			Class.forName("com.mysql.jdbc.Driver");
+			//连接本地数据库
+			conn =DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//获取连接并返回
+	public static Connection getConnection (){
+		return conn;
+	}
+}
+
+
